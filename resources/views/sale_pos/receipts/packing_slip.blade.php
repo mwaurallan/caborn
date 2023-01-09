70b31e6a8f67ce2fce1332107d921a2231d3e176
mwaurallan <allanmwaura2013@gmail.com>
2023-01-09 11:20:55 +0300

    changes

--- resources/views/sale_pos/receipts/packing_slip.blade.php
+++ resources/views/sale_pos/receipts/packing_slip.blade.php
@@ -30,7 +30,7 @@
 
 		<!-- Shop & Location Name  -->
 		@if(!empty($receipt_details->display_name))
-			<p>
+			<p class="text-left font-23 color-555>
 				{{$receipt_details->display_name}}
 				@if(!empty($receipt_details->address))
 					<br/>{!! $receipt_details->address !!}
